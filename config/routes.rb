7804aaf5334d8b153ce1471031cea7dd9cf6a0bc
Shane Becker <veganstraightedge@gmail.com>
2011-06-26 22:07:42 -0700

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 HomesteadingStatus::Application.routes.draw do
   resources :statuses, :except => [:update]
+  root :to => "statuses#index"
 
 
 
@@ -45,8 +46,4 @@ HomesteadingStatus::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 end
