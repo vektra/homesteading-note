248ce69baff7e2b2c3af1d3fa134264a118a7aac
Shane Becker <veganstraightedge@gmail.com>
2014-07-16 18:10:23 -0700

    Add (c) copyright to site footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def license_copy
+    first_post_year = Object.const_get(setting(:post_type).classify).first.published_at.year
+    years           = Time.now.year
+
+    unless years == first_post_year
+      years = "#{first_post_year}&ndash;#{years}"
+    end
+
+    "Copyright &copy; #{years} #{setting :author_name}.".html_safe
+  end
 end
