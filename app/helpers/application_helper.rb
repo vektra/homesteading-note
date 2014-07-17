a16ab978e0e1b587721f69b4250d44d746c5b11c
Shane Becker <veganstraightedge@gmail.com>
2014-07-16 23:59:35 -0700

    Show year range on license declaration in footer, not just this year

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,7 @@ module ApplicationHelper
   def license_text
     output = []
 
-    first_post      = Object.const_get(setting(:post_type).classify).first
-    first_post_year = first_post.published_at.year
-
-    years = Time.now.year
-    if Time.now.year == first_post_year
-      years = Time.now.year
-    else
-      years = "#{first_post_year}&ndash;#{Time.now.year}"
-    end
-
+    # license
     license = License.find(setting(:license))
 
     if license.name == "All Rights Reserved"
@@ -22,7 +13,21 @@ module ApplicationHelper
       output << link_to("#{license.name} (#{license.short_code})", license.url, rel: "license")
     end
 
-    output << years
+
+    # range of years
+    years_range = Time.now.year
+
+    first_post      = Object.const_get(setting(:post_type).classify).last
+    first_post_year = first_post.nil? ? years_range : first_post.published_at.year
+
+    if first_post_year == years_range
+      output << years_range
+    else
+      output << "#{first_post_year}&ndash;#{years_range}"
+    end
+
+
+    # author's name
     output << setting(:author_name)
 
     output.join(" ").html_safe
